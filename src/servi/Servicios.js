211e124aad10ee575fbebf5a86e8e9744f93498a
jhonhtr <129766078+jhonhtr@users.noreply.github.com>
2024-10-22 16:56:05 -0500

    Update Servicios.js

--- src/servi/Servicios.js
+++ src/servi/Servicios.js
@@ -1,6 +1,9 @@
 import React, { useEffect } from 'react';
+import { faHome, faBars } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import './Servicios.css';
 import fondo from './imagen/fondo.jpg';
+import logo from './imagen/logo.png';
 
 const Servicios = () => {
     useEffect(() => {
@@ -17,16 +20,146 @@ const Servicios = () => {
 
     return (
         <div>        
-            <ul className="menu">
-                <li><a href="/">Inicio</a></li>
-                <li><a href="">Servicios</a></li>
-                <li><a href="/Contacto">Contacto</a></li>    
-            </ul>
+            <nav>
+                <input type="checkbox" name="" id="check"/>
+                <label htmlFor="check" className="checkbtn"> {/* Cambié for a htmlFor */}
+                    <FontAwesomeIcon icon={faBars} />
+                    {/*<FontAwesomeIcon icon={faHome} />*/}
+                </label>
+                <a href="#" className="enlace"> <img src={logo} alt="cama1" width={70} height={70} /> </a>                
+                <ul className="menu">                
+                    <li><a href="/">Inicio</a></li>
+                    <li><a href="">Servicios</a></li>
+                    <li><a href="/Contacto">Contacto</a></li>  
+                </ul>
+            </nav>
 
-            
+            <div className='serviciogeneral'>
+                
+                <div className='servicio1'>
+                    <div className="texto-servicio">
+                        Fotocopias a blanco y negro y color.
+                    </div>                    
+                </div>
+
+                <div className='servicio1'>
+                    <div className="texto-servicio">
+                        Elaboración y envío de diversos documentos tanto personales como de trabajo.
+                    </div>                    
+                </div>
+
+                <div className='servicio1'>
+                    <div className="texto-servicio">
+                        Asesorías en documentación ante entidades públicas y privadas.
+                    </div>                    
+                </div>
+
+                <div className='servicio1'>
+                    <div className="texto-servicio">
+                        Digitalización de textos, oficios, poderes, autorizaciones, derechos de petición, trámites virtuales, compraventas, testamentos.
+                    </div>                    
+                </div>
+            </div>
+
+            <div className='serviciogeneral'>
+                
+                <div className='servicio1'>
+                    <div className="texto-servicio">
+                        Impresión tanto láser como de inyección de tinta.
+                    </div>                    
+                </div>
+
+                <div className='servicio1'>
+                    <div className="texto-servicio">
+                        Búsqueda de trabajo y envío de Hoja de Vida.
+                    </div>                    
+                </div>
+
+                <div className='servicio1'>
+                    <div className="texto-servicio">
+                        Respaldo de archivos en CD, rotulación en CDs.
+                    </div>                    
+                </div>
+
+                <div className='servicio1'>
+                    <div className="texto-servicio">
+                    Uso y capacitación técnica de las aplicaciones más comunes del mercado.
+                    </div>                    
+                </div>
+            </div>
+
+            <div className='serviciogeneral'>
+                
+                <div className='servicio1'>
+                    <div className="texto-servicio">
+                        Expedición de certificados de libertad y tradición.
+                    </div>                    
+                </div>
+
+                <div className='servicio1'>
+                    <div className="texto-servicio">
+                        Apostille
+                    </div>                    
+                </div>
+
+                <div className='servicio1'>
+                    <div className="texto-servicio">
+                        Pagos virtuales PSE.
+                    </div>                    
+                </div>
+
+                <div className='servicio1'>
+                    <div className="texto-servicio">
+                        Anillados
+                    </div>                    
+                </div>
+            </div>
+
+            <div className='serviciogeneral'>
+                
+                <div className='servicio1'>
+                    <div className="texto-servicio">
+                        Laminación
+                    </div>                    
+                </div>
+
+                <div className='servicio1'>
+                    <div className="texto-servicio">
+                        Soporte técnico para computadores.
+                    </div>                    
+                </div>
+
+                <div className='servicio1'>
+                    <div className="texto-servicio">
+                        Instalación de redes e impresoras.
+                    </div>                    
+                </div>
+
+                <div className='servicio1'>
+                    <div className="texto-servicio">
+                        Todo esto apoyado por la venta de refrescos y artículos de papelería.
+                    </div>                    
+                </div>
+            </div>
 
             <footer>
-                
+                <div>                
+                    <div className='pie'>
+                        <h2>MUNDO SOLUCIONES NET</h2><br/>
+                        <h3>Horario de atención:<br/>
+                            Lunes de Viernes: 07:00am - 06:30pm<br/>
+                            Sábados: 08:00am - 06:30am<br/>
+                            <a className='contacto' target="_blank" rel="noopener noreferrer" 
+                            href="https://wa.me/+573124191663">+57 312 419 1663</a><br/>
+                            <a className='correo' targe="_black" rel="noopener noreferrer" href="mailto:mundosolucionesnet@gmail.com">mundosolucionesnet@gmail.com</a><br/>
+                            Carrera 12 # 13-82 Centro <br/>
+                            Florencia, Caquetá - Colombia <br/><br/>
+                            Desarrollador: Jhon Tierradentro / <a className='contacto' target="_blank" rel="noopener noreferrer" href="https://wa.me/+573205119854">+573205119854</a><br/>
+                            <a className='correo' target="_blank" rel="noopener noreferrer" href="mailto:jhonhtr@gmail.com">jhonhtr@gmail.com </a><br/>                
+                            Derechos Reservados- Copyright 2024
+                        </h3>
+                    </div>
+                </div>
             </footer>
         </div>
     );
