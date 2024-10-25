5406329da849b779afc32180e773ffb4db9e2b4b
jhonhtr <129766078+jhonhtr@users.noreply.github.com>
2024-10-25 15:07:24 -0500

    Update Contacto.js

--- src/contac/Contacto.js
+++ src/contac/Contacto.js
@@ -151,9 +151,9 @@ const Contacto = () => {
                             </a><br/>
 
                             {/*<a className='correo' target="_blank" rel="noopener noreferrer" href="mailto:jhonhtr@gmail.com">jhonhtr@gmail.com </a><br/>*/}
-                            <a className='correo' target="_blank" rel="noopener noreferrer" href="mailto:jhonhtr@gmail.com">
-                                <FontAwesomeIcon icon={faEnvelope} /> jhonhtr@gmail.com
-                            </a><br/>
+                            {/*<a className='correo' target="_blank" rel="noopener noreferrer" href="mailto:jhonhtr@gmail.com">*/}
+                                {/*<FontAwesomeIcon icon={faEnvelope} /> jhonhtr@gmail.com
+                            </a><br/>*/}
                             Derechos Reservados- Copyright 2024
                         </h3>
                     </div>                    
